5027a88683f29706b802ab63f805692f0bf6a87e
lawrencey99 <lawrencey99@gmail.com>
2015-04-04 09:07:50 -0700

    create website

--- website/app.js
+++ website/app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var port = process.env.PORT || 8080;
 //var stream = twit.stream('statuses/filter', { locations: world });
 
 app.use(express.static(__dirname + '/public'));
