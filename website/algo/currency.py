551a0e8fe7677793e83abfbdf008592055b04110
Walter Qian <walterqian@ucla.edu>
2015-04-04 23:05:47 -0700

    price algo

--- website/algo/currency.py
+++ website/algo/currency.py
@@ -1,1 +1,37 @@
 #!/usr/local/bin/python
+
+
+def priceSaved(user):
+    start = None 
+    end = None 
+    tmp = user
+    while (tmp.prev != None):
+        tmp = tmp.prev
+    
+    start = tmp 
+    tmp = end 
+
+    while (tmp.next != None):
+        tmp = tmp.next
+
+    end = tmp 
+    tmp = start
+    totalPrice = 0;
+    
+    while (tmp.next != None):
+        totalPrice += tmp.price; 
+
+    price = getPriceFromServer(start,end)
+    #finish above after database is setup
+
+    percentSaved = price/totalPrice
+    tmp = start
+    while (tmp.next != None):
+        tmp.savings = (tmp.price * percentSaved)
+
+
+
+
+
+
+        
